772df42ef21071f1611ba1c8e9e4d4ec7f3f665d
Isaac Lyons <isaac@snowskeleton.net>
2022-09-06 13:35:07 -0400

    Fixed pypi issue and now ready to upload.

--- README.md
+++ README.md
@@ -2,21 +2,21 @@
 The basic idea of YouNeedAMint is to set up a Mint account linked to your Apple Card to automatically import transactions, and then move those transactions from Mint to YNAB. For some reason, this is easier than simply linking directly from Apple Card to YNAB. If you're a developer from one or both of those companies, please make it happen.
 
 # Installation
-Ensure your have a recent version of Python installed.
 
-For an automated install, run 
+Available from PyPi:
 
-```curl -l https://raw.githubusercontent.com/snowskeleton/ynam/master/remote_install.sh > i.sh && bash i.sh```
+```pip install ynam```
 
-Or manually like:
+
+Manual install:
 
 ```git clone https://github.com/snowskeleton/ynam.git```
 
 ```cd ynam```
 
-```./install.sh```
+```pip install --upgrade .```
 
 
 Then simply
 
-```ynam quickstart```
+```ynam --quickstart```
--- setup.py
+++ setup.py
@@ -1,25 +1,34 @@
 from setuptools import setup, find_packages
-import pathlib
+import sys, re, pathlib
+from os import system
+
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == "publish":
+    system("python setup.py sdist bdist_wheel")
+    system("twine upload dist/*")
+    sys.exit()
 
 here = pathlib.Path(__file__).parent
+long_description = (here / "README.md").read_text("utf-8")
+about = (here / "src" / "ynam" / "_version.py").read_text("utf-8")
 
-long_description = (pathlib.Path(__file__).parent,
-                    "README.md").read_text("utf-8")
 
-setup(author='snowskeleton',
-      author_email='isaac@snowskeleton.net',
-      description='Mint to YNAB translator',
-      include_package_data=True,
-      keywords="ynab, mint",
-      license='Apache 2.0',
-      long_description_content_type="text/markdown",
-      long_descriptin=long_description,
-      name='ynam',
+def read_from_file(key):
+    return re.search(f"{key} = ['\"]([^'\"]+)['\"]", about).group(1)
+
+
+setup(name=read_from_file("__title__"),
+      version=read_from_file("__version__"),
       package_dir={"": "src"},
-      packages=find_packages("src"),
-      url='https://github.com/snowskeleton/ynam',
-      version='v0.1.1',
+      include_package_data=True,
+      description=read_from_file("__description__"),
+      url=read_from_file("__url__"),
+      license=read_from_file("__license__"),
+      author=read_from_file("__author__"),
+      author_email=read_from_file("__author_email__"),
+      long_description_content_type="text/markdown",
+      packages=["ynam"],
       install_requires=[
-          'mintapi',
+          "mintapi",
       ],
       entry_points={"console_scripts": ["ynam = ynam.main:run"]})
